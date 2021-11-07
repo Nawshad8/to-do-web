c5cd3c26f356d9331d7a2d28d27a3eeb31b0f911
Rafin Rahman <ra-fin@live.com>
2021-11-07 01:01:31 +0000

    UI layout todo

--- resources/views/components/main_page.blade.php
+++ resources/views/components/main_page.blade.php
@@ -1,14 +1,17 @@
 <div>
 
     <head>
-
         <!-- Styles -->
         <style>
-            
-            .round {
-                position: relative;
+
+            h1{
+                margin-right: 20px;
             }
 
+            .round {
+                position: relative;
+                margin-bottom: 80px;
+            }
             .round label {
                 background-color: #050A2E;
                 border: 2px solid #ccc;
@@ -20,7 +23,6 @@
                 top: 0;
                 width: 28px;
             }
-
             .round label:after {
                 border: 2px solid #fff;
                 border-top: none;
@@ -69,9 +71,21 @@
                 margin: 0 auto;
             }
 
-        </style>
+            .circle{
+                background-color: white;
+                height: 30px;
+                width: 30px;
+                border-radius: 50%;
 
-        <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+            }
+            .circle img{
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                padding-top: 2px;
+            }
+
+        </style>
 
     </head>
 
@@ -80,57 +94,52 @@
         <main>
 
             {{-- Title div --}}
-            <div class="title">
-                {{-- <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="suitcase" class="svg-inline--fa fa-suitcase" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M0 144v288C0 457.6 22.41 480 48 480H96V96H48C22.41 96 0 118.4 0 144zM336 0h-160C150.4 0 128 22.41 128 48V480h256V48C384 22.41 361.6 0 336 0zM336 96h-160V48h160V96zM464 96H416v384h48c25.59 0 48-22.41 48-48v-288C512 118.4 489.6 96 464 96z"></path></svg> --}}
-                <i class="fa-solid fa-suitcase"></i>
-                <img src="" alt="Suitcase">
-                <h2>Work</h2>
-                <img src="" alt="Vertical triple dots">
 
+                <img class="circle" src="https://img.icons8.com/material-outlined/24/000000/suitcase.png"/>
+                <h1>Work</h1>
 
-
-            </div>
 
             {{-- Today div --}}
             <div>
-                
                 <h2>Today</h2>
-
                 {{-- Triple #Checkbox --}}
-
-                <div class="container">
                     <div class="round">
-                      <input type="checkbox" checked id="checkbox" />
-                      <label for="checkbox">Pay internet</label>
+                      <input type="checkbox"  id="checkbox1" />
+                      <label for="checkbox1">Pay internet</label>
                     </div>
+
+                <div class="round">
+                    <input type="checkbox"  id="checkbox2" />
+                    <label for="checkbox2">Get the documents from home</label>
                 </div>
 
-                {{-- NOTE : If using multiple checkboxes it checks in and out only the first one of the list --}}
-
-                <p> Get the documents from home</p>
-                <p>Charge my watch</p>
-
+                <div class="round">
+                    <input type="checkbox"  id="checkbox3" />
+                    <label for="checkbox3">Charge my watch</label>
+                </div>
             </div>
 
             {{-- Tomorrow div --}}
             <div>
-                
                 <h2>Tomorrow</h2>
-
                 {{-- #Checkbox --}}
-
-                <p>Reelase cr.fetbiko.ru</p>
-
+                <div class="round">
+                    <input type="checkbox"  id="checkbox4" />
+                    <label for="checkbox4">Reelase cr.fetbiko.ru</label>
+                </div>
             </div>
 
             {{-- Next week div --}}
             <div>
-                
+
                 <h2>Next Week</h2>
 
                 {{-- #Checkbox --}}
 
-                <p>Release todo app</p>
+                <div class="round">
+                    <input type="checkbox"  id="checkbox5" />
+                    <label for="checkbox5">Release todo app</label>
+                </div>
 
             </div>
 
