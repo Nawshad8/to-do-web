16e6656e33b154eab1864177b67af939b05cd757
Nawshad Rahman <nawshadrahman8@gmail.com>
2021-11-20 14:24:51 +0000

    start checklist mainpage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
             margin-top: 20px;
         }
 
-        .container {
+        .container-side {
             display: flex;
             max-width: 90%;
             margin-left: auto;
@@ -63,7 +63,7 @@
         }
 
         ul li:hover {
-            background-color: black;
+            background-color: #7653F2;
             padding-left: 10px;
             transition: 0.5s;
         }
@@ -104,11 +104,84 @@
             background-color: #050A2E;
         }
 
+/*--------------------------------------------------*/
+
+
+        /* Customize the label (the container) */
+        .container-checkbox {
+            display: block;
+            position: relative;
+            padding-left: 35px;
+            margin-bottom: 12px;
+            cursor: pointer;
+            font-size: 22px;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        /* Hide the browser's default checkbox */
+        .container-checkbox input {
+            position: absolute;
+            opacity: 0;
+            cursor: pointer;
+            height: 0;
+            width: 0;
+        }
+
+        /* Create a custom checkbox */
+        .checkmark {
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 25px;
+            width: 25px;
+            background-color: #eee;
+        }
+
+        /* On mouse-over, add a grey background color */
+        .container-checkbox:hover input ~ .checkmark {
+            background-color: #ccc;
+        }
+
+        /* When the checkbox is checked, add a green background */
+        .container-checkbox input:checked ~ .checkmark {
+            background-color: #7FFF00;
+        }
+
+        /* Create the checkmark/indicator (hidden when not checked) */
+            .checkmark:after {
+            content: "";
+            position: absolute;
+            display: none;
+        }
+
+        /* Show the checkmark when checked */
+        .container-checkbox input:checked ~ .checkmark:after {
+            ;
+        }
+
+        /* Style the checkmark/indicator */
+        .container-checkbox .checkmark:after {
+            left: 9px;
+            top: 5px;
+            width: 5px;
+            height: 10px;
+            border: solid white;
+            border-width: 0 3px 3px 0;
+            -webkit-transform: rotate(45deg);
+            -ms-transform: rotate(45deg);
+            transform: rotate(45deg);
+        }
+
+
+
     </style>
 </head>
 
 <body>
-<div class="container">
+<div class="container-side">
     <div class="side-bar">
         <div class="logo">
             <img src="https://img.icons8.com/material/24/000000/mysql-logo.png"/>
@@ -139,7 +212,47 @@
         </div>
     </div>
     <div class="main-page">
-        main page
+
+                <img class="circle" src="https://img.icons8.com/material-outlined/24/000000/suitcase.png"/>
+                <h1>Work</h1>
+    
+                <div>
+
+                    <h2>Today</h2>
+
+                    <label class="container-checkbox"> Pay internet
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+
+                    <label class="container-checkbox"> Get the documents from home
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+                        
+                    <label class="container-checkbox"> Charge my watch
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+    
+                    https://www.w3schools.com/howto/howto_css_custom_checkbox.asp
+
+                <div>
+                    <h2>Tomorrow</h2>
+                    <div class="round">
+                        <input type="checkbox"  id="checkbox4" />
+                        <label for="checkbox4">Reelase cr.fetbiko.ru</label>
+                    </div>
+                </div>
+    
+
+                <div>
+                    <h2>Next Week</h2>  
+                    <div class="round">
+                        <input type="checkbox"  id="checkbox5" />
+                        <label for="checkbox5">Release todo app</label>
+                    </div>
+                </div>
     </div>
 </div>
 
